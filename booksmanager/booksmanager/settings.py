6b6446f3e6e62d1ca5260c16f789bccf15e12c7a
Alicho220 <jimmyoalicho@gmail.com>
2022-09-08 02:57:28 +0100

    remove django-heroku that's causing problems in heroku

--- booksmanager/booksmanager/settings.py
+++ booksmanager/booksmanager/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +132,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
