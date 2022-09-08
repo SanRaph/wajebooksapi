4ea1f7ce74d6afad26796cbd5285b772989a9f95
Alicho220 <jimmyoalicho@gmail.com>
2022-09-08 02:54:27 +0100

    modified settings to add django_heroku

--- booksmanager/booksmanager/settings.py
+++ booksmanager/booksmanager/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
