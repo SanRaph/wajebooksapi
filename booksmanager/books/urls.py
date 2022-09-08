ff4180fc0e404ef9eb86cbe31cc9f91dc612ad30
Alicho220 <jimmyoalicho@gmail.com>
2022-09-08 07:31:52 +0100

    makes changes

--- booksmanager/books/urls.py
+++ booksmanager/books/urls.py
@@ -2,8 +2,8 @@ from rest_framework import routers
 from .api import AuthorViewSet, BookViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register('authors', AuthorViewSet, 'authors')
+router.register('/authors', AuthorViewSet, 'authors')
 
-router.register('books', BookViewSet, 'books')
+router.register('/books', BookViewSet, 'books')
 
 urlpatterns = router.urls
